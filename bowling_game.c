d02271579387209a95e94a1937d899d33a5f1622
Brian <brian@brianjolly.com>
2011-06-15 20:26:04 -0700

    First passing test\!

--- bowling_game.c
+++ bowling_game.c
@@ -1,11 +1,14 @@
 #include "bowling_game.h"
 
+static int score;
+
 void bowling_game_init() {
 }
 
 void bowling_game_roll(int pins) {
+    score = 0;
 }
 
 int bowling_game_score() {
-    return -1;
+    return score;
 }
