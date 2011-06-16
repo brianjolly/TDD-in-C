865844873bb9e3f22fad05df66de4492b22ac9c7
Brian <brian@brianjolly.com>
2011-06-15 20:42:27 -0700

    Improved design tests still passing

--- bowling_game.c
+++ bowling_game.c
@@ -1,14 +1,22 @@
 #include "bowling_game.h"
 
-static int score;
+enum { max_rolls = 21 };
+static int rolls[max_rolls];
+static int current_roll;
 
 void bowling_game_init() {
+    for (int i=0; i<max_rolls; i++)
+        rolls[i] = 0;
+    current_roll = 0;
 }
 
 void bowling_game_roll(int pins) {
-    score += pins;
+    rolls[current_roll++] = pins;
 }
 
 int bowling_game_score() {
+    int score = 0;
+    for (int i=0; i<max_rolls; i++)
+        score += rolls[i];
     return score;
 }
